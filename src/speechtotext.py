2b40866ac8b24f835cbf8affcbb6c3fcc9a00d21
Cole Dewis <coledewis@gmail.com>
2024-01-06 15:44:14 -0700

    real time sorta works

--- src/speechtotext.py
+++ src/speechtotext.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+import queue
 
 mic_idx = None
 for device_index, device_name in sr.Microphone.list_working_microphones().items():
@@ -8,12 +9,26 @@ for device_index, device_name in sr.Microphone.list_working_microphones().items(
 if mic_idx is None:
     raise ValueError("mic not found")
 
+data_queue = queue.Queue()
 mic = sr.Microphone(mic_idx)
 recognizer = sr.Recognizer()
+recognizer.dynamic_energy_threshold = False
+recognizer.energy_threshold = 1000
+
+def listen_callback(_, audio: sr.AudioData):
+    data_queue.put(audio)
 
 with mic as source:
     recognizer.adjust_for_ambient_noise(source)
-    print("TALK!")
-    data = recognizer.listen(source, phrase_time_limit=1.0)
-    print(data)
-    print(recognizer.recognize_whisper(data, model="tiny.en"))
+    
+print("Starting!")
+recognizer.listen_in_background(source=source, phrase_time_limit=1.0, callback=listen_callback)
+
+while True:
+    if not data_queue.empty():
+        while not data_queue.empty():
+            data = data_queue.get()
+            text = recognizer.recognize_whisper(data, model="medium.en")
+            print(text)
+    
+    

2cdbfd14dd901b7f767eed07d04fdda72739b74f
Akemi Izuko <akemi@noway.moe>
2024-01-06 12:46:06 -0700

    Llama: api connection

--- src/main.py
+++ src/main.py
@@ -1,1 +1,43 @@
-print("Hello blahaj")
+import requests as rq
+import json
+import time
+import re
+
+#print("Hello blahaj")
+
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+}
+
+prompt = """
+Please replace all the words in the following list with "#BEEP#" in the text delimited by triple back ticks. You are not allowed to output any additional explanation. Be direct to the point. Only output the censored sentence, nothing more. You cannot add any notes after!
+
+ - oranges
+ - pears
+ - pear
+
+```
+I got some pairs of oranges for my pear.
+"""
+
+data = {
+    "model": "mistral:instruct",
+    "prompt": prompt,
+}
+
+data = json.dumps(data)
+
+start = time.time()
+response = rq.post('http://localhost:11434/api/generate', headers=headers, data=data)
+cease = time.time()
+
+#print(f"Took: {cease-start}s")
+
+json_version = re.sub(r"\n(?=.)", ",", response.text)
+json_version = f"[{json_version}]"
+json_version = re.sub(f"\n", "", json_version)
+
+print(json_version)
+js = json.loads(json_version)
+
+#print(js)

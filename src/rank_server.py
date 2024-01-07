b036668ad70f3f904c63edb389f63b84cded2790
Akemi Izuko <akemi@noway.moe>
2024-01-07 05:25:11 -0700

    Merge in audio trimmer to server

--- src/rank_server.py
+++ src/rank_server.py
@@ -4,6 +4,7 @@ import requests as rq
 import json
 import time
 import re
+import inflect
 import random
 
 import numpy as np
@@ -11,6 +12,13 @@ import torch
 import whisper
 import struct
 import threading
+
+import torchaudio
+import torchaudio.functional as F
+import wave
+import io
+import pydub
+from pydub import AudioSegment
 
 NUM_ROUNDS = 5
 
@@ -21,6 +29,13 @@ PORT_WEB = 9033
 WEB_RESPONSE = "[]".encode('utf-8')
 
 headers = { "Content-Type": "application/x-www-form-urlencoded" }
+
+def pluralize(w):
+    if not inflect.singular_noun(w):
+        return (w, inflect.plural(w))
+    else:
+        return (inflect.singular_noun(w), w)
+
 
 def query_model(data):
     response = rq.post("http://localhost:11434/api/generate", headers=headers, data=data)
@@ -74,6 +89,96 @@ def recvall(sock, n):
     return data
 
 
+def align(emission, tokens):
+    targets = torch.tensor([tokens], dtype=torch.int32, device="cuda")
+    alignments, scores = F.forced_align(emission, targets, blank=0)
+    alignments, scores = alignments[0], scores[0]
+    scores = scores.exp()
+    return alignments, scores
+
+
+def unflatten(list_, lengths):
+    assert len(list_) == sum(lengths)
+    i = 0
+    ret = []
+    for l in lengths:
+        ret.append(list_[i : i + l])
+        i += l
+    return ret
+
+
+def get_word_starts(words):
+    words_index = list()
+
+    running_sum = 0
+    for word in words.split():
+        words_index.append([running_sum, word, False])
+        running_sum += len(word) + 1
+
+    return words_index
+
+
+def to_running_index(words_index):
+    running_index = [-1 for _ in range(words_index[-1][0] + len(words_index[-1][1]))]
+
+    for i in range(len(running_index)):
+        for ii, word in enumerate(words_index):
+            start = word[0]
+            cease = word[0] + len(word[1]) - 1
+
+            if start <= i <= cease:
+                running_index[i] = ii
+
+    return running_index
+
+def word_starts_to_string(word_starts):
+    s = list()
+
+    for word in word_starts:
+        if word[2]:
+            s.append(word[1])
+        else:
+            s.append("#BEEP#")
+
+    return " ".join(s)
+
+def sequence_match(lhs, rhs, word_starts, word_run_idx)
+    matcher = difflib.SequenceMatcher(None, lhs, rhs)
+
+    for x in matcher.get_matching_blocks()[:-1]:
+        i, j, n = x
+
+        while words1[i] == " ":
+            i += 1
+            j += 1
+            n -= 1
+
+        for x in range(n):
+            x += i
+            index = word_run_idx[x]
+
+            if index != -1:
+                word_starts[index][2] = True
+
+    return word_starts
+
+
+def censor_words(audio, beep_wav, word_spans, word_starts, ratio, sample_rate):
+    prev_rm_word_idx = -1
+    audio_buffer = audio[:0]
+
+    new_audio = audio[:]
+
+    for i in range(len(word_starts)):
+        if not word_starts[i][2]:  # Don't include this word
+            start = 1000 * int(word_spans[i][0].start * ratio) / sample_rate
+            cease = 1000 * int(word_spans[i][-1].end * ratio) / sample_rate
+
+            new_audio = new_audio[:start] + beep_wav[:cease-start] + new_audio[cease:]
+
+    return new_audio
+
+
 def web_backend_thread():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT_WEB))
@@ -98,7 +203,24 @@ def web_backend_thread():
 ############################################################
 # Main
 ############################################################
+
+##########################
+# Startup Constants
+##########################
 model = whisper.load_model("large").to("cuda")
+inflect = inflect.engine()
+beep_sound = AudioSegment.from_wav("beep1.wav")
+
+bundle = torchaudio.pipelines.MMS_FA
+LABELS = bundle.get_labels(star=None)
+DICTIONARY = bundle.get_dict(star=None)
+
+censor_words = list()
+with open("bad-words.csv", "r") as f:
+    for line in f.readlines():
+        word = line.strip()
+        censor_words.append(pluralize(word)[0])
+        censor_words.append(pluralize(word)[1])
 
 web_thread = threading.Thread(target=web_backend_thread)
 web_thread.start()
@@ -120,14 +242,31 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
                 audio_np = np.frombuffer(og_audio, dtype=np.int16).astype(np.float32) / 32768.0
                 results = model.transcribe(audio_np, fp16=True)
-                transcribed_text = results["text"]
+                og_transcribed_text = results["text"]
+                processed_words = str(og_transcribed_text)
+
+                ##########################
+                # Regex Filtering
+                ##########################
+                for word in censor_words:
+                    processed_words = re.sub(
+                        rf"\b{word}\b", "#BEEP#", processed_words, flags=re.IGNORECASE
+                    )
+
+                processed_words = re.sub("(\s*#BEEP#\s*)+", " #BEEP# ", processed_words)
+
+                # Alignment (used for audio alignment later)
+                removed_words = re.sub("#BEEP#", "", processed_words)
+                word_starts = get_word_starts(removed_words)
+                word_run_idx = to_running_index(word_starts)
+                word_starts = sequence_match(og_transcribed_text, removed_words, word_starts, word_run_idx)
 
                 ##########################
                 # Prompting
                 ##########################
                 data = {
                     "model": "openorca_hacked:v0.7",
-                    "prompt": transcribed_text,
+                    "prompt": processed_words,
                 }
                 data = json.dumps(data)
 
@@ -151,6 +290,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 print("==== LLAMA =============")
                 print(response.decode('utf-8'))
                 print("========================")
+
+                ##########################
+                # Chop up
+                ##########################
+                waveform, _ = torchaudio.load(io.BytesIO(og_audio))
+                transcript = "shawty got them apple bottom jeans boots with the fur".split()
+                with torch.inference_mode():
+                    emission, _ = model(waveform.to("cuda"))
+
+                tokenized_transcript = [DICTIONARY[c] for word in transcript for c in word]
+                aligned_tokens, alignment_scores = align(emission, tokenized_transcript)
+                token_spans = F.merge_tokens(aligned_tokens, alignment_scores)
+                word_spans = unflatten(token_spans, [len(word) for word in transcript])
+
+                ratio = waveform.size(1) / emission.size(1)
+                audio = AudioSegment.from_wav(io.BytesIO(og_audio))
+
+                new_audio = censor_audio(audio, beep_sound, word_spans, word_starts, ratio, bundle.sample_rate)
+                new_audio.export("out.wav", format="wav")
+
+                ##########################
+                # Respond
+                ##########################
                 msg = (
                     struct.pack('>I', len(og_audio))
                     + og_audio
